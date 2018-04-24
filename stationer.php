b208fe51c6d221f900f5d3adceb53cdf6a5398fb
Jenniefer <jennieferbylin@hotmail.com>
2018-04-24 10:14:11 +0200

    fixat lite i kartan

--- stationer.php
+++ stationer.php
@@ -1,39 +1,158 @@
 <!DOCTYPE html>
-<html>
+<html>	
+
 <head>
-	<title>Återvinning nära dig</title>
-	<link rel="stylesheet" href="css/style.css" type="text/css">
+<link rel="stylesheet" href="css/style.css" type="text/css">
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <title>Återvinningsstationer</title>
+    <style>
 
-</head>
-<body>
+      #map {
+	  height: 80%
+	  }
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 10;
+      }
+ #description {
+        font-family: Roboto;
+        font-size: 15px;
+        font-weight: 300;
+      }
+
+      #infowindow-content .title {
+        font-weight: bold;
+      }
+
+      #infowindow-content {
+        display: none;
+      }
+
+      #map #infowindow-content {
+        display: inline;
+      }
+
+      .pac-card {
+        margin: 10px 10px 0 0;
+        border-radius: 2px 0 0 2px;
+        box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        outline: none;
+        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+        background-color: #fff;
+        font-family: Roboto;
+      }
+
+      #pac-container {
+        padding-bottom: 12px;
+        margin-right: 12px;
+      }
+
+      .pac-controls {
+        display: inline-block;
+        padding: 5px 11px;
+      }
+
+      .pac-controls label {
+        font-family: Roboto;
+        font-size: 13px;
+        font-weight: 300;
+      }
+
+      #pac-input {
+        background-color: #fff;
+        font-family: Roboto;
+        font-size: 15px;
+        font-weight: 300;
+        margin-left: 12px;
+        padding: 0 11px 0 13px;
+        text-overflow: ellipsis;
+        width: 400px;
+      }
+
+      #pac-input:focus {
+        border-color: #4d90fe;
+      }
+
+      #title {
+        color: #fff;
+        background-color: #4d90fe;
+        font-size: 25px;
+        font-weight: 500;
+        padding: 6px 12px;
+      }
+      #target {
+        width: 345px;
+      }
+    </style>
+  </head>
+
+  <body>
+
+  <h3>Återvinningsstationer </h3>
+
+    <input id="pac-input" class="controls" type="text" placeholder="Search Box">
+    <div id="map">  <?php 
+include'includes/header.php';
+
+
+include'includes/mainmenu.php';
+
+?></div>
 	
-	<?php 
-include("includes/header.php");
+    <script>
+
+      function initMap() {
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 9,
+          center: {lat: 62.390811, lng: 17.306926999999973}
+        });
+
+        // Create an array of alphabetical characters used to label the markers.
+        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        // Add some markers to the map.
+        // Note: The code uses the JavaScript Array.prototype.map() method to
+        // create an array of markers based on a given "locations" array.
+        // The map() method here has nothing to do with the Google Maps API.
+        var markers = locations.map(function(location, i) {
+          return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+          });
+        });
+
+        // Add a marker clusterer to manage the markers.
+        var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+      }
+      var locations = [
+        {lat: 62.3883269, lng: 17.315367000000037},
+		{lat: 62.3086771, lng: 17.375304099999994},
+        {lat: 62.4398423, lng: 17.38845379999998},
+		{lat: 62.41875099999999, lng: 17.344162900000015},
+		{lat: 62.39455820000001, lng: 17.25011500000005}
+      ]
+	 
+	  var input = document.getElementById('pac-input');
+	  var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+		 map.addListener('bounds_changed', function() {
+          searchBox.setBounds(map.getBounds());
+        });
 
 
-include("includes/mainmenu.php");
+    </script>
+    <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js">
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDlPHpelqZ4zFYixbA9EtXR9FJXVzwA-ag&callback=initMap">
+    </script>
 
-
-
-
-?>
-	<h1> Karta </h1>
-
-	<p>
-		<img src="css/karta.jpg" class="img"/>
-	</p>
-
-	<p> Text om kartan?? </p>
-
-
-
- 
-
-<?php
-
-include("includes/footer.php");
-
-?>
 
 </body>
 </html>
