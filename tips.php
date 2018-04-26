595ceff6669295da9ed6c57c42849696082e44b9
Filip Nyberg <filipnyberg@filips-air.eduroam.miun.se>
2018-04-26 10:19:27 +0200

    tips

--- tips.php
+++ tips.php
@@ -17,7 +17,19 @@ include("includes/mainmenu.php");
 ?>
 	<h1> Här ger vi er några tips kring avfallshantering </h1>
 
-	<p> det är super att tänka på avfallshantering för det påverkar vår miljö </p>
+	
+	<h1>Sopor är värdefulla!</h1>
+	
+	<p> 
+
+Förpackningar av olika slag, hushållspapper, kartonger, den lästa tidningen, använda batterier, matavfall och tomflaskor. 
+
+Varje dag producerar Sverige en massa skräp. I Sverige drygt 500 kg per person och år. Sorterar vi rätt så kommer dessa sopor att komma till användning igen, som nya produkter. Avfallen runt om i världen ökar med flera procent varje år viket absolut inte är hållbart om vi inte gör det vi kan för att återvinna detta på rätt sätt. 
+
+Vad tjänar vi då på att återvinna?? 
+
+Jo, Den viktigaste effekten av sortering är att vi minskar på produktionen av nytt material. Då sparar vi energi och andra naturresurser och minskar också utsläppen av föroreningar i luft och vatten.
+Ett exempel är förpackningar av aluminum. Genom att återvinna dessa istället för att producera nytt så sparar vi ca 95% energi.  </p>
 
 
 
