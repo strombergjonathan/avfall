bfc15f48be80a8add6a5737f88623556c2a41ffc
Jenniefer <jennieferbylin@hotmail.com>
2018-04-26 09:35:39 +0200

    kartan

---
+++ googlekarta.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+
+   </head>
+<body>
+<?php 
+include("includes/header.php");
+
+
+include("includes/mainmenu.php");
+
+
+
+
+?>
--- stationer.php
+++ stationer.php
@@ -1,10 +1,10 @@
-<!DOCTYPE html>
-<html>	
 
-<head>
-<link rel="stylesheet" href="css/style.css" type="text/css">
+<!DOCTYPE html>
+<html>
+  <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
+	<link rel="stylesheet" href="css/style.css" type="text/css">
     <title>Återvinningsstationer</title>
     <style>
 
@@ -16,13 +16,7 @@
         margin: 0;
         padding: 10;
       }
- #description {
-        font-family: Roboto;
-        font-size: 15px;
-        font-weight: 300;
-      }
-
-      #infowindow-content .title {
+	   #infowindow-content .title {
         font-weight: bold;
       }
 
@@ -33,75 +27,16 @@
       #map #infowindow-content {
         display: inline;
       }
-
-      .pac-card {
-        margin: 10px 10px 0 0;
-        border-radius: 2px 0 0 2px;
-        box-sizing: border-box;
-        -moz-box-sizing: border-box;
-        outline: none;
-        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
-        background-color: #fff;
-        font-family: Roboto;
-      }
-
-      #pac-container {
-        padding-bottom: 12px;
-        margin-right: 12px;
-      }
-
-      .pac-controls {
-        display: inline-block;
-        padding: 5px 11px;
-      }
-
-      .pac-controls label {
-        font-family: Roboto;
-        font-size: 13px;
-        font-weight: 300;
-      }
-
-      #pac-input {
-        background-color: #fff;
-        font-family: Roboto;
-        font-size: 15px;
-        font-weight: 300;
-        margin-left: 12px;
-        padding: 0 11px 0 13px;
-        text-overflow: ellipsis;
-        width: 400px;
-      }
-
-      #pac-input:focus {
-        border-color: #4d90fe;
-      }
-
-      #title {
-        color: #fff;
-        background-color: #4d90fe;
-        font-size: 25px;
-        font-weight: 500;
-        padding: 6px 12px;
-      }
-      #target {
-        width: 345px;
-      }
     </style>
   </head>
-
   <body>
+  <p>
+   <iframe width="100%" height="500" src="googlekarta.php"
+    frameborder="0" allowfullscreen></iframe>
+    </p>
 
-  <h3>Återvinningsstationer </h3>
-
-    <input id="pac-input" class="controls" type="text" placeholder="Search Box">
-    <div id="map">  <?php 
-include'includes/header.php';
-
-
-include'includes/mainmenu.php';
-
-?></div>
-	
+  <h2>Återvinningsstationer </h2>
+    <div id="map"></div>
     <script>
 
       function initMap() {
@@ -130,29 +65,17 @@ include'includes/mainmenu.php';
             {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
       }
       var locations = [
-        {lat: 62.3883269, lng: 17.315367000000037},
+      {lat: 62.3883269, lng: 17.315367000000037},
 		{lat: 62.3086771, lng: 17.375304099999994},
         {lat: 62.4398423, lng: 17.38845379999998},
 		{lat: 62.41875099999999, lng: 17.344162900000015},
 		{lat: 62.39455820000001, lng: 17.25011500000005}
       ]
-	 
-	  var input = document.getElementById('pac-input');
-	  var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-		 map.addListener('bounds_changed', function() {
-          searchBox.setBounds(map.getBounds());
-        });
-
-
     </script>
     <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js">
     </script>
     <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDlPHpelqZ4zFYixbA9EtXR9FJXVzwA-ag&callback=initMap">
     </script>
-
-
-</body>
+  </body>
 </html>
